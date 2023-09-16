04940f3814b79bad0e7e43c375f30d861ea13f8c
ben05allen <ben05allen@hotmail.com>
2023-09-16 16:35:49 +0900

    updating results

--- Sep20to24.py
+++ Sep20to24.py
@@ -3,41 +3,18 @@ from static import Country, Ground
 
 FULL_TABLE = True
 
-table = {
-    Country.IRELAND: 91.82,
-    Country.SOUTH_AFRICA: 91.37,
-    Country.FRANCE: 89.90,
-    Country.NEW_ZEALAND: 88.38,
-    Country.SCOTLAND: 83.72,
-    Country.ENGLAND: 81.59,
-    Country.AUSTRALIA: 80.82,
-    Country.WALES: 79.46,
-    Country.ARGENTIA: 79.22,
-    Country.FIJI: 79.08,
-    Country.SAMOA: 76.19,
-    Country.ITALY: 75.63,
-    Country.GEORGIA: 75.28,
-    Country.JAPAN: 73.29,
-    Country.TONGA: 70.29,
-    Country.PORTUGAL: 68.61,
-    Country.URUGUAY: 66.63,
-    Country.USA: 66.22,
-    Country.ROMANIA: 64.56,
-    Country.SPAIN: 64.05,
-    Country.NAMIBIA: 61.61,
-    Country.CHILE: 60.49,
-}
+table = {...}
 
 
 results = [
-    ((Country.URUGUAY, 12, Ground.AWAY), (Country.FRANCE, 27, Ground.HOME)),
-    ((Country.NEW_ZEALAND, 71, Ground.AWAY), (Country.NAMIBIA, 3, Ground.AWAY)),
-    ((Country.SAMOA, 0, Ground.AWAY), (Country.CHILE, 0, Ground.AWAY)),
-    ((Country.WALES, 0, Ground.AWAY), (Country.PORTUGAL, 0, Ground.AWAY)),
-    ((Country.IRELAND, 0, Ground.AWAY), (Country.TONGA, 0, Ground.AWAY)),
-    ((Country.SOUTH_AFRICA, 0, Ground.AWAY), (Country.ROMANIA, 0, Ground.AWAY)),
-    ((Country.AUSTRALIA, 0, Ground.AWAY), (Country.FIJI, 0, Ground.AWAY)),
-    ((Country.ENGLAND, 0, Ground.AWAY), (Country.JAPAN, 0, Ground.AWAY)),
+    ((Country.ITALY, 0, Ground.AWAY), (Country.URUGUAY, 0, Ground.AWAY)),
+    ((Country.NAMIBIA, 0, Ground.AWAY), (Country.FRANCE, 0, Ground.HOME)),
+    ((Country.SAMOA, 0, Ground.AWAY), (Country.ARGENTIA, 0, Ground.AWAY)),
+    ((Country.GEORGIA, 0, Ground.AWAY), (Country.PORTUGAL, 0, Ground.AWAY)),
+    ((Country.ENGLAND, 0, Ground.AWAY), (Country.CHILE, 0, Ground.AWAY)),
+    ((Country.IRELAND, 0, Ground.AWAY), (Country.SOUTH_AFRICA, 0, Ground.AWAY)),
+    ((Country.SCOTLAND, 0, Ground.AWAY), (Country.TONGA, 0, Ground.AWAY)),
+    ((Country.AUSTRALIA, 0, Ground.AWAY), (Country.WALES, 0, Ground.AWAY)),
 ]
 
 old_ranking = {
